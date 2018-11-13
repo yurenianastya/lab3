8d7caaf94b2d226f064b3aaf4958d0ffe0fc169d
Nastya <43321431+yurenianastya@users.noreply.github.com>
2018-11-13 11:52:57 +0200

    Minor fixes, renamed variables etc

--- main.c
+++ main.c
@@ -3,52 +3,55 @@
 
 
 int main() {
-    int array[5][5] = {{1, 1, 6, 7, 1},{5, 3, 2, 5, 9},{4, 6, 2, 4, 3},{2, 5, 6, 4, 5},{3, 4, 6, 9, 3}};
-    for(int i = 0; i < 5; i++)
+    int Matrix[5][5] = {{1, 1, 6, 7, 1},
+                        {5, 3, 2, 5, 9},
+                        {4, 6, 2, 4, 3},
+                        {2, 5, 6, 4, 5},
+                        {3, 4, 6, 9, 3}};
+    for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
         {
-            printf("%d\t", array[i][j]);
+            printf("%d\t", Matrix[i][j]);
         }
         printf("\n");
     }
-    int i, k, j, x;
+    int i, k, j, tempMatrix; // these are varibables for the loops
     for (j = 0; j < 5 ; j++)
     {
         for (k = 1; k < 5 ; k++)
         {
-            x = array[k][j];
-            for (i = k - 1; i >= 0 && array[i][j] < x; i--)
+            tempMatrix = Matrix[k][j];
+            for (i = k - 1; i >= 0 && Matrix[i][j] < tempMatrix; i--)
             {
-                array[i + 1][j] = array[i][j];
-                array[i][j] = x;
+                Matrix[i + 1][j] = Matrix[i][j];
+                Matrix[i][j] = tempMatrix;
             }
         }
     }
-        printf ("\n\n");
+    printf ("\n\n");
     for(int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            printf("%d\t", array[i][j]);
+            printf("%d\t", Matrix[i][j]);
         }
         printf("\n");
     }
     double product = 1.0;
-    double gav = 1.0;
-    double suma = 0.0;
+    double geometricAverageValue = 1.0;
+    double sumOfAverageValues = 0.0;
     for ( i = 1; i < 5 ; i++){
         for ( j = 0; j < 5; j++){
             if (i > j) {
-                product*=array[i][j];
+                product*=Matrix[i][j];
             }
         }
-        gav = pow(product,1/4.0);
+        geometricAverageValue = pow(product,1/4.0);
         printf("i=%d\t",i);
-        printf("gav=%lf\n",gav);
-        suma +=gav;
-        gav = 1.0;
+        printf("geometricAverageValue=%lf\n",geometricAverageValue);
+        sumOfAverageValues +=geometricAverageValue;
         product = 1.0;
     }
-    printf("suma=%lf",suma);
+    printf("sumOfAverageValues=%lf",sumOfAverageValues);
 
     return 0;
-    }
+}
